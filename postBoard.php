9c756e8db16b7e5fa378d52d25d4119d9dc5e97e
Michael Ildefonso <michaelildefonso8@gmail.com>
2025-07-03 12:51:21 +0800

    aaaa

---
+++ postBoard.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post">
+        <h2>Post to Board</h2>
+        <label for="title">Title</label>
+        <input type="text" id="title" name="title" required>
+
+        <label for="content">Content</label>
+        <textarea id="content" name="content" required></textarea>
+
+        <label for=""></label>
+    </form>
+    
+</body>
+</html>
