1acabf989101f4a5a9959a163518367ec189b3e5
kenncarrido <kctdk0221@gmail.com>
2025-07-03 14:37:57 +0800

    Fix Display bug while filtering on homepage

--- home.php
+++ home.php
@@ -99,22 +99,5 @@
             <?php
         }
         ?>
-        <div class="home-article">
-            <img src="styles/images/article-sample.png" alt="Article Main Image" class="home-article-image">
-            <div>
-                <div class="article-title">Welcome to ACLC Blogs!</div>
-                <div class="article-meta">By Admin | 2024-06-01</div>
-                <div class="article-content">This is the first post on ACLC Blogs. Stay tuned for more updates and articles!</div>
-            </div>
-        </div>
-        <div class="home-article">
-            <img src="styles/images/article-sample.png" alt="Article Main Image" class="home-article-image">
-            <div>
-                <div class="article-title">Getting Started with Blogging</div>
-                <div class="article-meta">By Jane Doe | 2024-06-02</div>
-                <div class="article-content">Learn how to start your own blog and share your thoughts with the world.</div>
-            </div>
-        </div>
-    </div>
 </body>
 </html>
