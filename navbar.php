cad98ff4c2db6563807b82f13b1b52b74fe3f74e
Michael Ildefonso <michaelildefonso8@gmail.com>
2025-07-10 13:27:46 +0800

    logout button in navbar

--- navbar.php
+++ navbar.php
@@ -1,3 +1,11 @@
+<?php
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['logout'])) {
+    session_unset();
+    session_destroy();
+    header("Location: login.php");
+    exit;
+}
+?>
 <nav class="navbar">
     <div class="nav-logo">
         <img src="styles/images/aclc-logo.png" alt="ACLC Logo" id="aclc-logo">
@@ -6,5 +14,10 @@
     <div>
         <a href="home.php">Home</a>
         <a href="articleList.php">Post Board</a>
+        <p>
+            <form method="POST" class="logout-form">
+                <button type="submit" name="logout">Logout</button>
+            </form>
+            </p>
     </div>
 </nav>
