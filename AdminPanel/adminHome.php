5be2fee3adb6e94f752e043b841d575ce8c72500
kenncarrido <kctdk0221@gmail.com>
2025-07-11 09:53:50 +0800

    admin panel

---
+++ AdminPanel/adminHome.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <il>
+        <a href="postApprovalPage">Post Approval</a>
+        <a href="articleReq">Article Request</a>
+        <a href="postApprovalPage">Post Approval</a>
+        <a href="postApprovalPage">Post Approval</a>
+    </il>
+</body>
+</html>
