973e4cacf40151c2b68c97bdecde59f0236e5eaa
ikigaishini <aclcctaytay@gmail.com>
2025-11-10 10:55:56 +0800

    change config

--- backend/config.php
+++ backend/config.php
@@ -1,8 +1,8 @@
 <?php
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'blog');
-define('DB_USER', 'blog');
-define('DB_PASS', 'blog');
+define('DB_NAME', 'u472100509_blogs');
+define('DB_USER', 'u472100509_bloguser');
+define('DB_PASS', 'ClcTyty2512!');
  
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
